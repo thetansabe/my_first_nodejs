61a5dbd0181fb9c04f7bcdc7073a581a25ca5c86
thetansabe <thetannguyen193@gmail.com>
2021-12-28 10:44:30 +0700

    set up environment success

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 const path = require('path');
 const express = require('express');
-const { exphdb } = require("express-handlebars");
+const exphdb = require("express-handlebars");
 const morgan = require('morgan');
 
 const app = express()
 const port = 4420
 
+//import nhung file tinh ben trong folder public
+app.use(express.static(path.join(__dirname, 'public')))
 //HTTP logger
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('handlebars', exphdb())
-app.set('view engine','handlebars')
+app.engine('.hbs', exphdb({extname: '.hbs'}))
+app.set('view engine','.hbs')
 app.set('views', path.join(__dirname,'resources','views'))
 //__dirname la thu muc chua index.js => src
 
 //Main
 app.get('/', (req,res) => {
-    res.send('home')
+    res.render('home')
+})
+
+app.get('/news', (req,res) => {
+    res.render('news')
 })
 
 app.listen(port, () => {
